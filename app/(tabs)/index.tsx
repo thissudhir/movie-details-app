a2911649292826ae83b64548b528f722d1698a2a
Abhishek Yadav <abhishekworkhere@gmail.com>
2025-11-28 01:18:42 +0530

    FIX: hooks path for the custom hooks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchPopularMovies } from "@/services/api";
-import useFetch from "@/services/useFetch";
+import useFetch from "@/hooks/useFetch";
 import { useRouter } from "expo-router";
 import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
